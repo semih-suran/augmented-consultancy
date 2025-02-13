9288e930559bc3d210517a304cdb01c74d0b7360
Semih Suran <semih.suran@gmail.com>
2025-02-13 19:00:38 +0000

    Sphere using Phone data

--- src/scenes/start.js
+++ src/scenes/start.js
@@ -4,6 +4,7 @@ import {
   FreeCamera,
   Vector3,
   MeshBuilder,
+  WebXRHitTest,
 } from "babylonjs";
 
 export async function startScene(engine) {
@@ -13,7 +14,24 @@ export async function startScene(engine) {
   const cam = new FreeCamera("cam", new Vector3(0, 0, -2), scene);
   cam.attachControl();
 
-  const box = MeshBuilder.CreateBox("box", { size: 0.5 }, scene);
+  const dot = MeshBuilder.CreateSphere("dot", { diameter: 0.5 }, scene);
+
+  const xr = await scene.createDefaultXRExperienceAsync({
+    uiOptions: { sessionMode: "immersive-ar" },
+  });
+
+  const fm = xr.baseExperience.featuresManager;
+  const hitTest = fm.enableFeature(WebXRHitTest, "latest");
+
+  hitTest.onHitTestResultObservable.add((result) => {
+    if (result.length) {
+      result[0].transformationMatrix.decompose(
+        dot.scaling,
+        dot.rotationQuaternion,
+        dot.position
+      );
+    }
+  });
 
   await scene.whenReadyAsync();
 
