6546d289f18f449ded758ddb83db80a7fc5bbccc
Semih Suran <semih.suran@gmail.com>
2025-02-13 17:43:48 +0000

    First cube object in scene

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-import * as BABYLON from "babylonjs"
+import { Engine, Scene } from "babylonjs";
+import { startScene } from "./scenes/start.js";
 
-console.log(BABYLON);
+const engine = new Engine(document.querySelector("canvas"), true);
+
+async function main() {
+  const scene = await startScene(engine);
+
+  engine.runRenderLoop(() => scene.render());
+  window.addEventListener("resize", () => engine.resize());
+}
+
+main();
